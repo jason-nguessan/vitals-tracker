83ea0d42d595cb42966de09a1ae08c1746429eff
orr1nuycruz <orr1n@hotmail.com>
2020-12-19 20:27:03 -0500

    edits on the scan page

--- lib/mainPages/scanVitals.dart
+++ lib/mainPages/scanVitals.dart
@@ -10,6 +10,7 @@ class ScanVitals extends StatefulWidget {
 
 class _ScanVitalsState extends State<ScanVitals> {
   bool _toggled = false;
+
   _toggle() {
     setState(() {
       _toggled = true;
@@ -27,21 +28,32 @@ class _ScanVitalsState extends State<ScanVitals> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Center(
-          child: IconButton(
-            icon: Icon(_toggled ? Icons.favorite : Icons.favorite_border),
-            color: Colors.red,
-            iconSize: 128,
-            onPressed: () {
-              if (_toggled) {
-                _untoggle();
-              } else {
-                _toggle();
-              }
-            },
+          child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(color: Colors.red),
           ),
-        ),
-      ),
+          Expanded(
+            child: Center(
+              child: IconButton(
+                icon: Icon(_toggled ? Icons.favorite : Icons.favorite_border),
+                color: Colors.red,
+                iconSize: 128,
+                onPressed: () {
+                  if (_toggled) {
+                    _untoggle();
+                  } else {
+                    _toggle();
+                  }
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(color: Colors.black),
+          )
+        ],
+      )),
     );
   }
 }
